f75a030f1bfc90c79a094d78b08b433cff839291
Paula Weibel <paula.weibel@stud.hslu.ch>
2019-12-05 10:51:30 +0100

    inserted to html

--- _01-Generated/00_02/sketch.js
+++ _01-Generated/00_02/sketch.js
@@ -1,25 +1,21 @@
-var elem = document.getElementById('draw-animation');
-var two = new Two({ width: 285, height: 200 }).appendTo(elem);
+// Make an instance of two and place it on the page.
+var elem = document.getElementById('draw-shapes');
+var params = { width: 285, height: 200 };
+var two = new Two(params).appendTo(elem);
 
-var circle = two.makeCircle(-70, 0, 50);
-var rect = two.makeRectangle(70, 0, 100, 100);
+// two has convenience methods to create shapes.
+var circle = two.makeCircle(72, 100, 50);
+var rect = two.makeRectangle(213, 100, 100, 100);
+
+// The object returned has many stylable properties:
 circle.fill = '#FF8000';
-rect.fill = 'rgba(0, 200, 255, 0.75)';
+circle.stroke = 'orangered'; // Accepts all valid css color
+circle.linewidth = 5;
 
-var group = two.makeGroup(circle, rect);
-group.translation.set(two.width / 2, two.height / 2);
-group.scale = 0;
-group.noStroke();
+rect.fill = 'rgb(0, 200, 255)';
+rect.opacity = 0.75;
+rect.noStroke();
 
-// Bind a function to scale and rotate the group
-// to the animation loop.
-two.bind('update', function(frameCount) {
-  // This code is called everytime two.update() is called.
-  // Effectively 60 times per second.
-  if (group.scale > 0.9999) {
-    group.scale = group.rotation = 0;
-  }
-  var t = (1 - group.scale) * 0.125;
-  group.scale += t;
-  group.rotation += t * 4 * Math.PI;
-}).play();  // Finally, start the animation loop
+// Don't forget to tell two to render everything
+// to the screen
+two.update();
