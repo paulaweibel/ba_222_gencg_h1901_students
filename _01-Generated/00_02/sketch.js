4fe7b474e7a32292f7ceec8747c39f47f895c794
Paula Weibel <paula.weibel@stud.hslu.ch>
2019-12-05 11:18:24 +0100

    Update sketch.js

--- _01-Generated/00_02/sketch.js
+++ _01-Generated/00_02/sketch.js
@@ -2,6 +2,7 @@
 var elem = document.getElementById('draw-shapes');
 var params = { width: 285, height: 200 };
 var two = new Two(params).appendTo(elem);
+console.log("funtkionierts?")
 
 // two has convenience methods to create shapes.
 var circle = two.makeCircle(72, 100, 50);
