ac2cc2be8c9e6e3bd9d4b4f2dec6e0d3c63b5ec4
Paula Weibel <paula.weibel@stud.hslu.ch>
2019-12-18 13:51:10 +0100

    remove unuseful

--- _01-Generated/trashschwarzweissraupe/sketch.js
+++ _01-Generated/trashschwarzweissraupe/sketch.js
@@ -5,48 +5,34 @@
 var b = 255, p = false;
 var z = 0;
 let xoff = 0.0;
+let wid, wid2;
 
 function setup() {
   // Canvas setup
   canvas = createCanvas(windowWidth, windowHeight);
- // canvas.parent("p5Container");
-  // Detect screen density (retina)
   var density = displayDensity();
-
   pixelDensity(density);
-  // Colors and drawing modes
-  background(200);
   smooth();
-   // Canvas full page
-  createCanvas(windowWidth, windowHeight);
-  // Default screen density (for retina)
-  pixelDensity(density);
-  // Var init
   background(0);
   position = createVector(0, 0);
   velocity = createVector(2, 4);
-  smooth();
+  wid = windowHeight/2; //size of outer ellipse 
+  wid2 = windowHeight/2.1;//size of inner ellipse
   position.x=width/2;
   position.y=height/2;
+
   // Init Var
 }
 
 function draw() {
-  // background(0,50)
   smooth();
   noFill();
   stroke(0.1)
-  // position.x=width/2;
-  // position.y=height/2;
-
   position.add(velocity);
-
-
 
   //noise
   xoff = xoff + 0.05;
   let n = noise(xoff) * width/10;
-  //line(n, 0, n, height);
 
   //  Check for bouncing
   if ((position.x > width-(width/10)) || (position.x < (width/10))) {
@@ -55,43 +41,36 @@ function draw() {
   if ((position.y > height-(height/10)) || (position.y < (height/10))) {
     velocity.y = velocity.y * -1;
   }
-  // Display at x,y location
-  strokeWeight(1+(n/70));
-  stroke(0+n,50)
-
+  // Display of ellipses
+  //dark outer ellipse
   strokeWeight(10);
-  stroke(20,200)
-  ellipse(position.x,position.y,100-n,100-n);
-  fill(20,0,0);
-  noFill()
+  stroke(20,50)
+  ellipse(position.x,position.y,wid-n,wid-n);
+  //bright inner ellipse
+  fill(0,20);
   strokeWeight(2);
   stroke(250)
-  //rect(position.x-(120-n)/2,position.y-(120-n)/2,120-n,120-n);
-  ellipse(position.x,position.y,102-n,102-n);
-
-
+  ellipse(position.x,position.y,wid2-n,wid2-n);
 }
+
 function keyPressed() {
   if (keyCode === 32) setup() // 32 = Space
   if (keyCode === 38) direction = 'up' // 38 = ArrowUp
   if (keyCode === 40) direction = 'down' // 40 = ArrowDown
   if (keyCode >= 48 && keyCode <= 57) rideDuration = getRideDuration(toInt(key)) // 48...57 = Digits
-  //
   if (key === 's' || key === 'S') saveThumb(650, 350);
-//  console.log(getRideDuration(toInt(key)))
 }
+
 // Thumb
 function saveThumb(w, h) {
   let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
   save(img, 'thumb.jpg');
 }
 
-
 // Timestamp
 function timestamp() {
   return Date.now();
 }
-
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
