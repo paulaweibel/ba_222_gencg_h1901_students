f220ded504d2919bcbd489fde368c314c4c0497f
Paula Weibel <paula.weibel@stud.hslu.ch>
2019-12-18 13:42:40 +0100

    Revert "please work"
    This reverts commit 157eb604f9b73baf7ee7df4c19053aff1830ba63.

--- sketch.js
+++ sketch.js
@@ -4,12 +4,6 @@
 // Some of the var might be initialised in gui.js
 var canvas, backgroundGrey, radius;
 var actRandomSeed, count, points, increment;
-var b = 255, p = false;
-var z = 0;
-let xoff = 0.0;
-let wid; // size of outer ellipse
-let wid2; // size of inner ellipse
-
 
 function setup() {
   // Canvas setup
@@ -27,13 +21,6 @@ function setup() {
   background(backgroundGrey);
   radius = 20;
   increment = +1;
-  position = createVector(0,0);
-  velocity = createVector(2, 4);
-  wid = windowHeight; // size of outer ellipse
-  wid2 = windowHeight/1.5;  // size of inner ellipse
-  position.x=width/2;
-  position.y=height/2;
-  background(0);
 }
 
 function draw() {
@@ -54,7 +41,6 @@ function draw() {
     let x = width/2 + cos(angle*i)*radiusRand;
     let y = height/2 + sin(angle*i)*radiusRand;
     points[i] = createVector(x,y);
-
   }
 
   // Draw
@@ -71,40 +57,7 @@ function draw() {
     if (i==0 || i==count-1) curveVertex(points[i].x, points[i].y);
   }
   endShape(CLOSE);
-  smooth();
-  noFill();
-  stroke(0.1)
-  position.add(velocity);
-
-  //noise (for the changing size of ellipse)
-  xoff = xoff + 0.01;
-  let n = noise(xoff) * 1000;
-
-  //  Check for bouncing
-  if ((position.x > width-(width/10)) || (position.x < (width/10))) {
-    velocity.x = velocity.x * -1;
-  }
-  if ((position.y > height-(height/10)) || (position.y < (height/10))) {
-    velocity.y = velocity.y * -1;
-  }
-
-// size, stroke and colour of ellipses:
-  strokeWeight(5);
-  stroke(0,0,0,200)
-  ellipse(position.x,position.y,wid-n,wid-n);
-  strokeWeight(1);
-  stroke(300-(n/3),300-(n/2),300-(n/5))
-  ellipse(position.x,position.y,wid2-n,wid2-n);
 }
-
-function keyPressed() {
-  if (keyCode === 32) setup() // 32 = Space
-  if (keyCode === 38) direction = 'up' // 38 = ArrowUp
-  if (keyCode === 40) direction = 'down' // 40 = ArrowDown
-  if (keyCode >= 48 && keyCode <= 57) rideDuration = getRideDuration(toInt(key)) // 48...57 = Digits
-  if (key === 's' || key === 'S') saveThumb(650, 350);
-}
-
 
 function keyPressed() {
   if (key == DELETE || key == BACKSPACE) background(360);
