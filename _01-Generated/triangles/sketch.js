5bc1cac8af848ce8caa9c1e4c45b8e08639f2b58
Paula Weibel <paula.weibel@stud.hslu.ch>
2019-12-18 14:51:36 +0100

    homepage

--- _01-Generated/triangles/sketch.js
+++ _01-Generated/triangles/sketch.js
@@ -1,10 +1,4 @@
-// Global var
-let direction;
-let stepSize, rideDuration, startTime, t;
-let objects;
-
 function setup() {
-  cursor(HAND);
   particleStepMax = 50;
   initParticles();
   createCanvas(windowWidth, windowHeight);
@@ -14,20 +8,11 @@ function draw() {
   background(0,150);
   stroke(250,180,0,20)
   strokeWeight(2)
-
-  // Time since the sketch started
-  let t = (new Date() - startTime) / 1000;
-//  let stepSize = animate(t, 0, 2, rideDuration, 2.5)
-
-stepSize = (direction === 'up') ? +stepSize : -stepSize;
-
   particles.forEach(p => {
     p.move();
     p.draw();
   });
 }
-
-
 
 function Particle() {
   this.pos = createVector(random(windowWidth), random(windowHeight));
@@ -52,19 +37,6 @@ Particle.prototype.draw = function() {
 }
 
 
-function keyPressed() {
-  if (keyCode === 32) setup() // 32 = Space
-  if (keyCode === 38) direction = 'up' // 38 = ArrowUp
-  if (keyCode === 40) direction = 'down' // 40 = ArrowDown
-  if (keyCode >= 48 && keyCode <= 57) rideDuration = getRideDuration(toInt(key)) // 48...57 = Digits
-  //
-  if (key === 's' || key === 'S') saveThumb(650, 350);
-}
-// Thumb
-function saveThumb(w, h) {
-  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
-  save(img, 'thumb.jpg');
-}
 
 
 function initParticles() {
@@ -83,10 +55,6 @@ function mouseClicked() {
   background(255);
 }
 
-// resize canvas when the window is resized
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight, false);
-}
 
 // Int conversion
 function toInt(value) {
--- sketch.js
+++ sketch.js
@@ -18,9 +18,11 @@ function setup() {
   backgroundGrey = 0;
   count = 150;
   points = [count];
-  background(backgroundGrey);
+  background(200,20,200);
   radius = 20;
   increment = +1;
+  particleStepMax = 50;
+  initParticles();
 }
 
 function draw() {
@@ -33,7 +35,7 @@ function draw() {
   // let r = map(mouseY,0,height,10,radius);
   if (radius>width/1.7 && radius>height/1.7) increment = -increment;
   else if (radius<20) increment = -increment;
-  radius += increment; 
+  radius += increment;
   let angle = radians(360/count);
 
   for (let i=0; i<count; i++){
@@ -48,19 +50,57 @@ function draw() {
   strokeHsluv(noise(t/10)*360,noise(t/20)*50,noise(t)*80);
   strokeWeight(20);
   noFill();
-  beginShape();
-  for (let i=0; i<count; i++){
-    // fill(255);
-    // ellipse(points[i].x, points[i].y,2,2);
-    // noFill();
-    curveVertex(points[i].x, points[i].y);
-    if (i==0 || i==count-1) curveVertex(points[i].x, points[i].y);
-  }
-  endShape(CLOSE);
+  // beginShape();
+  // for (let i=0; i<count; i++){
+  //   // fill(255);
+  //   // ellipse(points[i].x, points[i].y,2,2);
+  //   // noFill();
+  //   curveVertex(points[i].x, points[i].y);
+  //   if (i==0 || i==count-1) curveVertex(points[i].x, points[i].y);
+  // }
+  // endShape(CLOSE);
+  background(0,150);
+  stroke(250,180,0,20)
+  strokeWeight(2)
+  particles.forEach(p => {
+    p.move();
+    p.draw();
+  });
 }
 
+function Particle() {
+  this.pos = createVector(random(windowWidth), random(windowHeight));
+  this.tail = [];
+  this.tailLength = 5;
+}
+
+Particle.prototype.move = function() {
+  if(this.tail.length > this.tailLength) {
+    this.tail.splice(0, 1);
+  }
+  this.tail.push(this.pos.copy());
+
+  this.pos.x += random(-particleStepMax, particleStepMax);
+  this.pos.y += random(-particleStepMax, particleStepMax);
+}
+
+Particle.prototype.draw = function() {
+  this.tail.forEach(pos => {
+    line(this.pos.x, this.pos.y, pos.x, pos.y);
+  });
+}
+
+
+function initParticles() {
+  particles = [];
+  for(var i = 0; i < 1000; i++) {
+    particles.push(new Particle());
+  }
+}
+
+
 function keyPressed() {
-  if (key == DELETE || key == BACKSPACE) background(360);  
+  if (key == DELETE || key == BACKSPACE) background(360);
   if (key == 's' || key == 'S') saveThumb(650, 350);
 }
 
